c24ddd95f0f0997a2abf1f1f7e2d46a409727900
arjun <arjun.vmaniyara@gmail.com>
2023-07-07 11:10:42 +0530

    api configration

--- FRONTEND/src/axios/axios.ts
+++ FRONTEND/src/axios/axios.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 const instance=Axios.create({
-    baseURL:'https://hirex.social/service',
+    baseURL:'https://hirex.social/api/v1/service',
     headers:{
     "Content-Type": "application/json"
     }
