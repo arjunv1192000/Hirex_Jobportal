7097999b8ace633453dd73001f0d5b4e2ed005c7
arjun <arjun.vmaniyara@gmail.com>
2023-07-08 18:52:23 +0530

    admin bug

--- FRONTEND/src/Admin/components/Graph.tsx
+++ FRONTEND/src/Admin/components/Graph.tsx
@@ -1,28 +1,39 @@
-
+import React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
-import { Box} from '@mui/material'
+import { Box } from '@mui/material';
 
 interface CompanyData {
+  _id: string;
   companyname: string;
   jobCount: number;
 }
 
-function Graph({ data }: { data: CompanyData[] }) {
-  if (!data) {
-   
+interface GraphProps {
+  data: CompanyData[];
+}
+
+function Graph({ data }: GraphProps) {
+  if (!data || !Array.isArray(data)) {
     return null;
   }
 
- 
-  const companyNames = data?.map((item) => item.companyname);
-  const jobCounts = data?.map((item) => item.jobCount);
+  const companyNames = data.map((item) => item.companyname);
+  const jobCounts = data.map((item) => item.jobCount);
   console.log(companyNames);
   console.log(jobCounts);
-  
-  
 
   return (
-    <Box sx={{ width: '80%', height: 'auto', borderRadius: 2, boxShadow: 6, backgroundColor: 'white', marginTop: 3, marginLeft:20 }}>
+    <Box
+      sx={{
+        width: '80%',
+        height: 'auto',
+        borderRadius: 2,
+        boxShadow: 6,
+        backgroundColor: 'white',
+        marginTop: 3,
+        marginLeft: 20,
+      }}
+    >
       <BarChart
         xAxis={[
           {
@@ -38,11 +49,10 @@ function Graph({ data }: { data: CompanyData[] }) {
         ]}
         width={900}
         height={600}
-        sx={{marginLeft:25}}
+        sx={{ marginLeft: 25 }}
       />
     </Box>
   );
 }
 
-
-export default Graph
+export default Graph;
